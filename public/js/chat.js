2dc924cfb17368fd731a97461a073073e843d9ec
dzonib <nikolabosnjak381@gmail.com>
2019-04-15 08:45:01 +0200

    started working on a chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,14 @@
 const socket = io()
 
-// with socket.on you recive event. There are two arguments:
-// 1. event name wich must match one you put in server on socket.emmit(),
-// and callback func wich gives you data you have put in socket.emmit , names dont have to match,
-// but order does)
-
-socket.on("countUpdated", countFromBackend => {
-    console.log("the count has been updated ", countFromBackend)
+socket.on('message', (data) => {
+    console.log(data)
 })
 
-document.querySelector("#increment-btn").addEventListener("click", () => {
-    console.log("I have incremented the count")
-    // on click emit event from client
-    socket.emit("increment")
+document.querySelector("#chat-form")
+    .addEventListener('submit', (e) => {
+    e.preventDefault()
+    const data = e.target.elements.message.value
+    socket.emit('chat-message', data)
 })
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketIo = require('socket.io')
+const path = require("path")
+const http = require("http")
+const express = require("express")
+const socketIo = require("socket.io")
 
 const app = express()
 
@@ -10,28 +10,20 @@ const server = http.createServer(app)
 const io = socketIo(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
-
+const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+io.on("connection", socket => {
+    console.log("New WebSocket connection!")
 
-io.on('connection', (socket) => {
-    console.log('New WebSocket connection!')
+    socket.emit("message", "Welcome!")
 
-    // create event and emit it to client
-    socket.emit('countUpdated', count)
-
-    // listen for increment event from client
-    socket.on('increment', () => {
-        // increment count and send current count fo client
-        count++
-        // socket.emit('countUpdated', count) this emits only to single user
-
-        io.emit('countUpdated', count) // this one emits to all connected users
+    socket.on("chat-message", chatMsg => {
+        io.emit("message", chatMsg)
     })
 })
 
-
-server.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
+server.listen(port, () =>
+    console.log(`Server is running on http://localhost:${port}`)
+)
